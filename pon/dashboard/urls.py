8c3b7230d111381c1b16cecfe8853f35bbe48ad9
Антон Езерский <anton030701@gmail.com>
2024-04-20 14:31:56 +0300

    event endpoint

--- pon/dashboard/urls.py
+++ pon/dashboard/urls.py
@@ -2,14 +2,14 @@ from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
 
-from .views import EventViewSet
+from .views import EventViewSet, TagViewSet
 
 router = DefaultRouter()
 
 router.register(r'events', EventViewSet, basename='events')
+router.register(r'tags', TagViewSet, basename='tags')
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('api/v1/', include(router.urls)),
 ]
 
-urlpatterns += router.urls
