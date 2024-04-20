fd1829db03cdabee05c0ffba3c95c794a916400c
mbuhtiarov <kotolow1@gmail.com>
2024-04-21 00:37:35 +0300

    forgotten files

--- pon/schedule/urls.py
+++ pon/schedule/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
 
-from .views import UserViewSet, TasksViewSet, set_task
+from .views import UserViewSet, TasksViewSet, set_task, get_calendar
 
 router = DefaultRouter()
 
@@ -11,6 +11,7 @@ router.register(r'tasks', TasksViewSet, basename='tags')
 
 urlpatterns = [
     path('api/v1/', include(router.urls)),
-    path('api/v1/set_task/', set_task, name='set_task'),
+    path('api/v1/set_task', set_task, name='set_task'),
+    path('api/v1/get_calendar', get_calendar, name='get_calendar'),
 ]
 
--- pon/schedule/views.py
+++ pon/schedule/views.py
@@ -1,9 +1,12 @@
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from db_app.serializers import UserSerializer, TasksSerializer
 from db_app.models import User, Tasks
-from .helpers import TasksHelper
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.db.models.functions import TruncDay
+from collections import defaultdict
+from django.db.models import Q
+import datetime
 
 
 class UserViewSet(ModelViewSet):
@@ -12,8 +15,6 @@ class UserViewSet(ModelViewSet):
 
 
 class TasksViewSet(ModelViewSet):
-    helper = TasksHelper()
-    helper.schedule_to_tasks(9370)
     queryset = Tasks.objects.all()
     serializer_class = TasksSerializer
 
@@ -24,3 +25,40 @@ def set_task(request):
     serializer.is_valid(raise_exception=True)
     serializer.save()
     return Response(serializer.data)
+
+
+@api_view(['GET'])
+def get_calendar(request):
+    print(request.query_params)
+    month = request.query_params.get('month')
+    year = request.query_params.get('year')
+
+    if month is None or year is None:
+        return Response({'error': 'Month and year parameters are required.'}, status=400)
+
+    try:
+        month = int(month)
+        year = int(year)
+
+        datetime.datetime(year=year, month=month, day=1)
+    except ValueError as e:
+        return Response({'error': str(e)}, status=400)
+
+    start_date = datetime.date(year, month, 1)
+    end_date = datetime.date(year, month + 1, 1) if month < 12 else datetime.date(year + 1, 1, 1)
+    queryset = Tasks.objects.filter(
+        Q(start_time__gte=start_date) & Q(start_time__lt=end_date)
+    ).annotate(day=TruncDay('start_time')).order_by('day')
+
+    tasks_by_day = defaultdict(list)
+    for task in queryset:
+        tasks_by_day[task.day.strftime('%Y-%m-%d')].append({
+            'taskid': task.taskid,
+            'name': task.name,
+            'description': task.description,
+            'done': task.done,
+            'start_time': task.start_time,
+            'end_time': task.end_time
+        })
+
+    return Response(tasks_by_day)
