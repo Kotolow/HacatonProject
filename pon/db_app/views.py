a24661cd3c88bea27fe58d8bcf71e0dbe6e5dcd6
Mikhal Bukhtiyarov <enbzhjd.vbfbk@yandex.ru>
2024-04-20 23:26:34 +0300

    view commit

--- pon/db_app/views.py
+++ pon/db_app/views.py
@@ -1,12 +1,15 @@
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from db_app.models import User
 from django import forms
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import authenticate
+import json
 
 
 class EmailLoginForm(AuthenticationForm):
-    username = forms.EmailField(label='email')
+    username = forms.EmailField(label='Email')
 
 
 class EmailSignUpForm(UserCreationForm):
@@ -30,17 +33,34 @@ def signup(request):
         return JsonResponse({'error': 'Метод запроса должен быть POST'})
 
 
+
+
+
+@csrf_exempt
 def login(request):
     if request.method == 'POST':
-        form = EmailLoginForm(request, request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            return JsonResponse({'success': 'Аутентификация успешна'})
-        else:
-            errors = form.errors.as_json()
-            return JsonResponse({'error': errors})
+        try:
+            data = json.loads(request.body)
+            email = data['email']
+            password = data['password']
+
+            # Добавим отладочный вывод для проверки полученных данных
+            print("Email:", email)
+            print("Password:", password)
+
+            # Используйте authenticate для аутентификации пользователя
+            user = authenticate(request, email=email, password=password)
+
+            if user is not None:
+                # Пользователь аутентифицирован успешно
+                return JsonResponse({'success': 'Аутентификация успешна'})
+            else:
+                # Неверные учетные данные
+                return JsonResponse({'error': 'Неверные учетные данные'}, status=400)
+        except KeyError:
+            return JsonResponse({'error': 'Отсутствуют необходимые данные'}, status=400)
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=500)
     else:
-        return JsonResponse({'error': 'Метод запроса должен быть POST'})
-
-
+        return JsonResponse({'error': 'Метод запроса должен быть POST'}, status=405)
 
